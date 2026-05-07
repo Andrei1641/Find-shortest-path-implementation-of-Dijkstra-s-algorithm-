eadc16d4f22db1763c4ea378f6b66cb6e7607b88
Andrei Skorofatov <andrijskorofatov11@gmail.com>
2026-05-07 17:59:18 +0200

    add start, finish, search shortest path

--- main.py
+++ main.py
@@ -24,11 +24,10 @@ clock: Clock = pygame.time.Clock()
 
 pos_nodes: dict[Node, tuple[int, int],] = {}
 NODE_RADIUS = 25
-C = (100, 10, 10)
+
 
 first_chosen_node: tuple[Node, tuple[int, int]] | None = None
 second_chosen_node: tuple[Node, tuple[int, int]] | None = None
-
 
 
 
@@ -36,6 +35,9 @@ typing_mode = 0
 towards_time = ""
 backwards_time = ""
 
+
+start_n: tuple[Node, tuple[int, int]] | None = None
+finish_n: tuple[Node, tuple[int, int]] | None = None
 
 stop = False
 while not stop:
@@ -61,18 +63,18 @@ while not stop:
                     pos_nodes[new_node] = mouse_pos
 
 
-            if event.key == pygame.K_k:
-                if first_chosen_node and second_chosen_node:
+            if event.key == pygame.K_RETURN and typing_mode == 0:
+                if (first_chosen_node and second_chosen_node) and (first_chosen_node != second_chosen_node):
                     typing_mode = 1
+                elif first_chosen_node == second_chosen_node or (bool(first_chosen_node) ^ bool(second_chosen_node)):
+                    typing_mode = 4
 
-
-            if event.key == pygame.K_RETURN and typing_mode == 1:
+            elif event.key == pygame.K_RETURN and typing_mode == 1:
                 typing_mode = 2
             elif event.key == pygame.K_RETURN and typing_mode == 2:
                 typing_mode = 3
 
             if typing_mode == 1:
-
                 if event.unicode.isdigit():
                     towards_time += event.unicode
 
@@ -82,23 +84,43 @@ while not stop:
                     backwards_time += event.unicode
 
             if typing_mode == 3:
-                if event.key == pygame.K_RETURN:
-                    print(first_chosen_node[1], int(towards_time), int(backwards_time))
+                if not (towards_time.isdigit() and backwards_time.isdigit()):
+                    second_chosen_node[0].add_path(first_chosen_node[0])
+                else:
                     second_chosen_node[0].add_path(first_chosen_node[0], int(towards_time), int(backwards_time))
-                    typing_mode = 0
+                typing_mode = 0
+
+
+            if typing_mode == 4 and not start_n:
+                start_n = first_chosen_node or second_chosen_node
+                typing_mode = 0
+
+            if typing_mode == 4 and start_n:
+                finish_n = first_chosen_node or second_chosen_node
+                typing_mode = 0
+
+            if event.key == pygame.K_s and (finish_n and start_n):
+                searcher = Searcher(start_n[0], list(pos_nodes.keys()))
+                searcher.find_shortest_path()
+                d = searcher.get_distances_dict()
+                print(d[finish_n[0]])
 
 
     for n, n_pos in pos_nodes.items():
-        pygame.draw.circle(screen, C, n_pos, NODE_RADIUS)
+        pygame.draw.circle(screen, (100, 10, 10), n_pos, NODE_RADIUS)
         for _, next_n in n.paths:
-            pygame.draw.line(screen, C, pos_nodes[n] , pos_nodes[next_n], 20)
+            pygame.draw.line(screen, (100, 10, 10), pos_nodes[n] , pos_nodes[next_n], 20)
 
 
 
     if first_chosen_node:
-        pygame.draw.circle(screen, C[1] + 100, first_chosen_node[1], NODE_RADIUS)
+        pygame.draw.circle(screen, (10, 100, 10), first_chosen_node[1], NODE_RADIUS)
     if second_chosen_node:
-        pygame.draw.circle(screen, C[1] + 100, second_chosen_node[1], NODE_RADIUS)
+        pygame.draw.circle(screen, (10, 100, 10), second_chosen_node[1], NODE_RADIUS)
+    if start_n:
+        pygame.draw.circle(screen, (100, 10, 100), pos_nodes[start_n[0]], NODE_RADIUS)
+    if finish_n:
+        pygame.draw.circle(screen, (100, 100, 10), pos_nodes[finish_n[0]], NODE_RADIUS)
 
 
     pygame.display.flip()
