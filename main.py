c2a1deba666d30ea43371a7824eed892f6d91fdf
Andrei Skorofatov <andrijskorofatov11@gmail.com>
2026-05-07 16:59:33 +0200

    draw connections

--- main.py
+++ main.py
@@ -7,40 +7,106 @@ from searcher import Searcher
 import pygame
 
 
+def mouse_node_collate(nodes_pos: dict[Node, tuple[int, int]], *, shift = 0) ->  tuple[Node, tuple[int, int]] | None:
+    mouse_pos = pygame.mouse.get_pos()
+    for node, pos in nodes_pos.items():
+        dist_to_mouse = ((pos[0] - mouse_pos[0]) ** 2 + (pos[1] - mouse_pos[1]) ** 2) ** 0.5
+        if dist_to_mouse < 2 * NODE_RADIUS + shift:
+            return node, pos
+    return None
+
+
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 
 clock: Clock = pygame.time.Clock()
 
-nodes_pos: dict[tuple[int, int], Node] = {}
+pos_nodes: dict[Node, tuple[int, int],] = {}
 NODE_RADIUS = 25
+C = (100, 10, 10)
+
+first_chosen_node: tuple[Node, tuple[int, int]] | None = None
+second_chosen_node: tuple[Node, tuple[int, int]] | None = None
+
+
+
+
+typing_mode = 0
+towards_time = ""
+backwards_time = ""
+
 
 stop = False
 while not stop:
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    first_chosen_node, second_chosen_node = mouse_node_collate(pos_nodes), first_chosen_node
+
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_p:
                 mouse_pos = pygame.mouse.get_pos()
-                dist_to_mouse = float('inf')
-                collate = False
-                for pos in nodes_pos:
-                    dist_to_mouse =  ((pos[0] - mouse_pos[0]) ** 2 + (pos[1] - mouse_pos[1]) ** 2) ** 0.5
-                    if dist_to_mouse < 2 * NODE_RADIUS + 10:
-                        collate = True
+                collate = mouse_node_collate(pos_nodes, shift=10)
 
                 if not collate:
-                    nodes_pos[mouse_pos] = Node()
-                    pygame.draw.circle(screen, (100, 10, 10), mouse_pos, NODE_RADIUS)
+                    new_node = Node()
+                    pos_nodes[new_node] = mouse_pos
+
+
+            if event.key == pygame.K_k:
+                if first_chosen_node and second_chosen_node:
+                    typing_mode = 1
+
+
+            if event.key == pygame.K_RETURN and typing_mode == 1:
+                typing_mode = 2
+            elif event.key == pygame.K_RETURN and typing_mode == 2:
+                typing_mode = 3
+
+            if typing_mode == 1:
+
+                if event.unicode.isdigit():
+                    towards_time += event.unicode
+
+
+            if typing_mode == 2:
+                if event.unicode.isdigit():
+                    backwards_time += event.unicode
+
+            if typing_mode == 3:
+                if event.key == pygame.K_RETURN:
+                    print(first_chosen_node[1], int(towards_time), int(backwards_time))
+                    second_chosen_node[0].add_path(first_chosen_node[0], int(towards_time), int(backwards_time))
+                    typing_mode = 0
+
+
+    for n, n_pos in pos_nodes.items():
+        pygame.draw.circle(screen, C, n_pos, NODE_RADIUS)
+        for _, next_n in n.paths:
+            pygame.draw.line(screen, C, pos_nodes[n] , pos_nodes[next_n], 20)
+
+
+
+    if first_chosen_node:
+        pygame.draw.circle(screen, C[1] + 100, first_chosen_node[1], NODE_RADIUS)
+    if second_chosen_node:
+        pygame.draw.circle(screen, C[1] + 100, second_chosen_node[1], NODE_RADIUS)
 
 
     pygame.display.flip()
     clock.tick(60)
+
+
+
+
 
 
 # a = Node()
